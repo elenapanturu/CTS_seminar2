abdeef2179c760c6144a855df917df17d7f1205d
Elena <elenapanturu29@gmail.com>
2022-03-01 19:02:48 +0200

    Initial commit

---
+++ src/classes/AngajatiReader.java
@@ -0,0 +1,30 @@
+package classes;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AngajatiReader implements ApplicantsReader {
+
+    @Override
+    public List<Aplicant> citireAplicanti(String numeFisier) throws FileNotFoundException {
+        Scanner input2 = new Scanner(new File(numeFisier));
+        input2.useDelimiter(",");
+        List<Aplicant> angajati = new ArrayList<Aplicant>();
+
+        while (input2.hasNext()) {
+            Angajat angajat = new Angajat();
+            ApplicantReader.citireAplicant(input2, angajat);
+            int salariu = input2.nextInt();
+            String ocupatie = input2.next();
+           angajat.setOcupatie(ocupatie);
+           angajat.setSalariu(salariu);
+            angajati.add(angajat);
+        }
+        input2.close();
+        return angajati;
+    }
+    }
+
---
+++ src/classes/ApplicantsReader.java
@@ -0,0 +1,8 @@
+package classes;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public interface ApplicantsReader {
+    List<Aplicant> citireAplicanti(String numeFisier) throws FileNotFoundException;
+}
